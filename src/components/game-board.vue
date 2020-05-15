ce248eed0534baf3735dd4fbe1c16967f0bf6e96
Stuart Jones <stuart@savvywombat.com.au>
2020-05-15 10:37:49 +0930

    grid

--- src/components/game-board.vue
+++ src/components/game-board.vue
@@ -1,9 +1,19 @@
 <template>
-  <h1>Game Board</h1>
+  <div>
+    <div v-for="x in width" style="display: inline-block">
+      <div v-for="y in height" style="border: 1px solid gray" :style="{ height: cellSize, width: cellSize }"></div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+      return {
+          height: 10,
+          width: 10,
+          cellSize: '40px'
+      }
+  }
 };
 </script>
